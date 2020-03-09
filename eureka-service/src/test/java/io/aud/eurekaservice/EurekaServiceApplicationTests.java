4268d636343ae0218f5ba095bcba4f8a9ad32964
Habraken,Noud N.H.H <n.habraken@student.fontys.nl>
2020-03-09 12:11:38 +0100

    Eureka server init

---
+++ eureka-service/src/test/java/io/aud/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aud.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
